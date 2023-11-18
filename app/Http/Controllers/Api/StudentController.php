98bb8fc325746f76d48c06aec767e098f97af0d7
boyarifsetiawan <boysetiawan5635@gmail.com>
2023-11-18 19:56:53 +0700

    Fungsi index,store dan show

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentRequest;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        if ($students->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Success',
+                'students' => $students
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Student not found.',
+                'students' => $students
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'erorrs' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+
+            if ($student) {
+                return response()->json([
+                    'status' => 200,
+                    'Message' => 'Student created succsessfully.'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'Message' => 'Something went wrong.'
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'Message' => 'Success',
+                'data' => $student
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'Message' => 'Something went wrong.'
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/students', [StudentController::class, 'index'])->name('student.index');
+Route::post('/students', [StudentController::class, 'store'])->name('student.store');
+Route::get('/students/{id}', [StudentController::class, 'show'])->name('student.show');
